43b1677c9277382b68ded599376367ecabf529e8
akshaya <akshaya@qburst.com>
2025-12-03 14:55:38 +0530

    added tags and meta data

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../Logo.svg"
+
+const Header = () => {
+    return(
+        <header>
+            <img src={logo} alt="Little Lemon"/>
+        </header>
+    )
+}
+
+export default Header;
